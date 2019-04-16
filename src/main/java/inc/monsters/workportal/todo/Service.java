8059b824e56ffd9d0010079bf1274a49348c687a
Trevor Curtis <tcurtis@waternoose.monsters.inc>
2019-04-16 19:01:43 -0400

    Commiting Maven'ified version.

---
+++ src/main/java/inc/monsters/workportal/todo/Service.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inc.monsters.workportal.todo;
+
+import java.util.List;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author tcurtis
+ */
+@Stateless
+public class Service {
+    private static final Logger LOG = Logger.getLogger(Service.class.getName());
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    /**
+     *
+     * @param _todo
+     */
+    public void add(Todo _todo) {
+        LOG.info("Service:getAll()");
+        em.merge(_todo);
+    }
+    
+    /**
+     *
+     * @return
+     */
+    public List<Todo> getAll() {
+        LOG.info("Service:getAll()");
+        return em.createQuery("SELECT t FROM Todo t", Todo.class).getResultList();
+    }
+    
+    public Todo get(Long _id) {
+        LOG.info("Service:get()");
+        return em.find(Todo.class, _id);
+    }
+}
---
+++ target/generated-sources/annotations/inc/monsters/workportal/todo/Todo_.java
@@ -0,0 +1,15 @@
+package inc.monsters.workportal.todo;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-16T18:51:01")
+@StaticMetamodel(Todo.class)
+public class Todo_ { 
+
+    public static volatile SingularAttribute<Todo, String> description;
+    public static volatile SingularAttribute<Todo, Long> id;
+    public static volatile SingularAttribute<Todo, String> title;
+
+}
